4e8578243f8f9a1c9069c82206ea683da1c3ab4f
Alemiglio98 <127412732+Alemiglio98@users.noreply.github.com>
2023-03-10 17:55:12 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -2,3 +2,15 @@
 # Let's instal the raster package
 instal.packages("raster")
 library(raster)
+#import data, setting the working directory
+setwd("C:/lab/")
+l2011<- brick("p224r63_2011_masked.grd")  
+plot(l2011) # plot of data 
+cl<-colorRampPalette(c("red","orange","yellow")) (100) #100 sono le sfumature
+plot(l2011,col= cl)
+#plotting one element 
+plot(l2011[[4]], col=cl)
+
+plot(l2011$B4_sre, col=cl)
+nir<- l2011[[4]]      #assegno la banda ad un singolo oggetto (uguale per il dollaro)
+plot (nir, col=cl)
